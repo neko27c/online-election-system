3854e48effad1b0e02a5fa51d7da882b2a5e936a
neko <wd27n1@gmail.com>
2026-01-27 11:52:18 +0900

    add .gitignore and remove cache and db

--- electionproject/settings.py
+++ electionproject/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "electionproject.onrender.com",
