f832a5e4a0227a6115bcaeaa2ad18ce0ac4ac29b
neko <wd27n1@gmail.com>
2026-01-28 09:36:05 +0900

    設定修正

--- electionproject/settings.py
+++ electionproject/settings.py
@@ -127,9 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = []
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'administrator', 'static'),
+    os.path.join(BASE_DIR, 'voter', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
