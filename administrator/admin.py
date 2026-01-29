db150839835da726f71e59e71d3aa71f5a92d57e
neko <wd27n1@gmail.com>
2026-01-29 09:38:28 +0900

    設定修正

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Administrator
+from .forms import AdministratorAdminForm
 
 # Register your models here.
 
 admin.site.register(Administrator)
+
+@admin.register(Administrator)
+class AdministratorAdmin(admin.ModelAdmin):
+    form = AdministratorAdminForm
--- administrator/forms.py
+++ administrator/forms.py
@@ -1,1 +1,16 @@
+# administrator/forms.py
 from django import forms
+from django.contrib.auth.hashers import make_password
+from .models import Administrator
+
+class AdministratorAdminForm(forms.ModelForm):
+    class Meta:
+        model = Administrator
+        fields = "__all__"
+
+    def clean_password(self):
+        pw = self.cleaned_data["password"]
+        # すでにハッシュならそのまま
+        if pw.startswith("pbkdf2_"):
+            return pw
+        return make_password(pw)
